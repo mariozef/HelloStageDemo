f72f2a999e628659b11ba2d9831e718791750e6f
mariozef <zefirr@o2.pl>
2016-11-05 11:43:01 +0100

    Pierwsze testy

---
+++ src/main/scala-2.12/demo/ScalaFXHelloWorld.scala
@@ -0,0 +1,13 @@
+package demo
+
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+
+/**
+  * Created by mariozef on 2016-11-05.
+  */
+object ScalaFXHelloWorld extends JFXApp{
+  stage = new PrimaryStage {
+    title = "ScalaFX Hello World"
+  }
+}
